0a5e3754b13ded9a693cee5ab9f4637c11ad1497
Kshitiz-2002 <kshitizpandey2002@gmail.com>
2023-12-02 00:05:16 +0530

    made publish page basic structure

--- src/components/PublishCard.js
+++ src/components/PublishCard.js
@@ -5,25 +5,6 @@ import './PublishCard.css';
 const PublishCard = () => {
  return (
     <div className="card-container">
-      <div className="card-header">Ride Details</div>
-      <div className="card-body">
-        <div className="card-info">
-          <div className="card-label">Start:</div>
-          <div className="card-content">Your starting location</div>
-        </div>
-        <div className="card-info">
-          <div className="card-label">Destination:</div>
-          <div className="card-content">Your destination</div>
-        </div>
-        <div className="card-info">
-          <div className="card-label">Taxi Number:</div>
-          <div className="card-content">ABC123</div>
-        </div>
-        <div className="card-info">
-          <div className="card-label">Time:</div>
-          <div className="card-content">12:30 PM</div>
-        </div>
-      </div>
     </div>
  );
 };
